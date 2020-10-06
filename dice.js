efbd48e39fa96bb80e1746cb0f43fcfa754bdb38
alpesh yadav <alpeshyadav2518@gmail.com>
2020-10-06 12:08:42 +0530

    refactored js file

--- dice.js
+++ dice.js
@@ -1,51 +1,52 @@
-var playerOne = Number.parseInt(1 + (Math.random() * 6));
+const playerOne = Number.parseInt(1 + (Math.random() * 6));
+const playerTwo = Number.parseInt(1 + (Math.random() * 6));
 
 if (playerOne == 1) {
     document.getElementById('img1').setAttribute("src", "images/dice1.png");
 }
-if (playerOne == 2) {
+else if (playerOne == 2) {
     document.getElementById('img1').setAttribute("src", "images/dice2.png");
 }
-if (playerOne == 3) {
+else if (playerOne == 3) {
     document.getElementById('img1').setAttribute("src", "images/dice3.png");
 }
-if (playerOne == 4) {
+else if (playerOne == 4) {
     document.getElementById('img1').setAttribute("src", "images/dice4.png");
 }
-if (playerOne == 5) {
+else if (playerOne == 5) {
     document.getElementById('img1').setAttribute("src", "images/dice5.png");
 }
-if (playerOne == 6) {
+else {
     document.getElementById('img1').setAttribute("src", "images/dice6.png");
 }
 
-var playerTwo = Number.parseInt(1 + (Math.random() * 6));
+
 
 if (playerTwo == 1) {
     document.getElementById('img2').setAttribute("src", "images/dice1.png");
 }
-if (playerTwo == 2) {
+else if (playerTwo == 2) {
     document.getElementById('img2').setAttribute("src", "images/dice2.png");
 }
-if (playerTwo == 3) {
+else if (playerTwo == 3) {
     document.getElementById('img2').setAttribute("src", "images/dice3.png");
 }
-if (playerTwo == 4) {
+else if (playerTwo == 4) {
     document.getElementById('img2').setAttribute("src", "images/dice4.png");
 }
-if (playerTwo == 5) {
+else if (playerTwo == 5) {
     document.getElementById('img2').setAttribute("src", "images/dice5.png");
 }
-if (playerTwo == 6) {
+else {
     document.getElementById('img2').setAttribute("src", "images/dice6.png");
 }
 
 if (playerOne > playerTwo) {
     document.getElementById("header").innerHTML = "<h1>Player 1 is the Winner!!</h1>"
 }
-if (playerOne < playerTwo) {
+else if (playerOne < playerTwo) {
     document.getElementById("header").innerHTML = "<h1>Player 2 is the Winner!!</h1>"
 }
-if (playerOne == playerTwo) {
+else {
     document.getElementById("header").innerHTML = "<h1>It's a Tie!</h1>"
 }
